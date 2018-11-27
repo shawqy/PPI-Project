f531d699928011f54bf2756bc5887eaeef270fe0
zyad mohamed <zezo19975@yahoo.com>
2018-11-27 23:08:54 +0200

    Update Port_B.v

--- Port_B.v
+++ Port_B.v
@@ -19,31 +19,16 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Port_B(
-   inout reg [7:0] B,
-inout reg [7:0] B_cpu,
+inout [7:0] B,
+inout [7:0] B_cpu,
 input group_control,// control on or out
 input control_logic // to activate the port
     );
+	 
+	 
+	 
+assign B = (!control_logic)? 8'bzzzz_zzzz : (!group_control)? B_cpu: 8'bzzzz_zzzz; // if group_control = 0 B is output
+assign B_cpu = (!control_logic)? 8'bzzzz_zzzz: (group_control)? B: 8'bzzzz_zzzz; // if control_group = 0 "reset" then B is input . if group_control = 1 B input  
 
-if (control_logic)
-begin
-		always @(group_control)
-		begin
-
-			if(!group_control)
-			begin
-				
-				B <= B_cpu;
-			
-			end
-			else begin
-				
-				B_cpu <= B;
-				
-			end
-
-		end //always
-
-end //if
 
 endmodule
