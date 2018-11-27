d05b8801ded9e7595240356378e20aaebdbee472
zyad mohamed <zezo19975@yahoo.com>
2018-11-27 23:08:48 +0200

    Update Group_B_control.v

--- Group_B_control.v
+++ Group_B_control.v
@@ -1,53 +1,54 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:40:37 11/22/2018 
-// Design Name: 
-// Module Name:    Group_A_control 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Group_B_control(
 input control_logic, //1 for control mode
-inout [6:0] bus, // D6,D5,D4,D3
-input M_S_F, // D7
+input [7:0] bus_cpu, // D6,D5,D4,D3
 output reg port_control_B,
-output reg port_control_C_L
-    );
+output reg port_control_C_L,
+output reg [3:0] bus,
+output reg BSR_mode
 
-always@(control_logic,bus)
+    );
+	 
+
+ 
+//assign bus_cpu = (!bus_cpu[7]&&control_logic)? bus : 8'bzzzz_zzzz;
+
+always@(control_logic,bus_cpu)
 		if (control_logic)
 		begin
 			
-			if(M_S_F)
+			if(bus_cpu[7])
 			begin //mode select flag
-				
-				case (bus[3])
+				BSR_mode <= 1'b0;
+				casez (bus_cpu[2])
 				
 					1'b0 : begin
-					Port_control_B <= (bus[1])? 1 : 0 ;
-					port_control_C_L <= (bus[0])? 1 : 0 ;
+					port_control_B <= (bus_cpu[1])? 1'b1 : 1'b0 ;
+					port_control_C_L <= (bus_cpu[0])? 1'b1 : 1'b0 ;
 					end
 									
 				endcase
 			
 			end //end of mode select
-			else begin //SBR
+		else begin //BSR
+			BSR_mode <= 1'b1;
+				casez (bus_cpu[3:1])
+					
+					3'b011: bus <=(bus_cpu[0])? 4'b1zzz :4'b0zzz;
+					3'b010: bus <=(bus_cpu[0])? 4'bz1zz :4'bz0zz;
+					3'b001: bus <=(bus_cpu[0])? 4'bzz1z :4'bzz0z;
+					3'b000: bus <=(bus_cpu[0])? 4'bzzz1 :4'bzzz0;
+					default:bus <=4'bzzzz;
+					
+				endcase
 			
-			
-			end //end of SBR
-			
+			end //end of BSR 
 		end // if
-
+   
+				
 endmodule
+
+
+
+
